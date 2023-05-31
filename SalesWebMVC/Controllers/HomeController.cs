ee1b0cd6025c7168bd5667a704b39926c5822dcf
ishma <ishmaelsan@gmail.com>
2023-05-31 15:39:50 -0300

    First controller and razor pages  tests

--- SalesWebMVC/Controllers/HomeController.cs
+++ SalesWebMVC/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMVC.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales WEB MVC app from C# Course";
+            ViewData["Student"] = "ishmael Jr";
 
             return View();
         }
