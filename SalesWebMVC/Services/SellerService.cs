f7a850d37f36f2b5787e37d56de2d43899656bbf
ishma <ishmaelsan@gmail.com>
2023-07-17 23:30:07 -0300

    SellerService and basic Findall

---
+++ SalesWebMVC/Services/SellerService.cs
@@ -0,0 +1,25 @@
+﻿using SalesWebMVC.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesWebMVC.Services;
+using SalesWebMVC.Models;
+
+namespace SalesWebMVC.Services
+{
+    public class SellerService
+    {
+        private readonly SalesWebMVCContext _context;
+
+        public SellerService(SalesWebMVCContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+    }
+}
